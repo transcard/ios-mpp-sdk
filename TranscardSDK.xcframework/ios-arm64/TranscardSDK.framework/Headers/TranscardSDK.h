18b4bbc753565620fb6e7b71423e85eecb780e9d
runner <runner@Mac-1675182855187.local>
2023-01-31 16:46:13 +0000

    Build 42026

---
+++ TranscardSDK.xcframework/ios-arm64/TranscardSDK.framework/Headers/TranscardSDK.h
@@ -0,0 +1,18 @@
+//
+//  TranscardSDK.h
+//  TranscardSDK
+//
+//  Created by Oscar De Moya on 11/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TranscardSDK.
+FOUNDATION_EXPORT double TranscardSDKVersionNumber;
+
+//! Project version string for TranscardSDK.
+FOUNDATION_EXPORT const unsigned char TranscardSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TranscardSDK/PublicHeader.h>
+
+
